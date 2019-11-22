12163b42b65f7a762b8e5c93360fb4dbf6b458fe
RitchieWhitemore <RichiB@yandex.ru>
2019-11-22 15:46:18 +0300

    Установил Eloquent-Sluggable

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.5.2 on 2019-11-22 11:10:48.
+ * Generated for Laravel 6.5.2 on 2019-11-22 12:36:17.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
