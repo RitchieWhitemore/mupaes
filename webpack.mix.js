22c78830e9d8da0064029f4be124a1665af2ecea
RitchieWhitemore <RichiB@yandex.ru>
2020-05-31 19:32:19 +0300

    Добавил шрифты

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,30 +14,33 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
+mix.copy('resources/fonts/*', 'public/fonts');
+
 mix.options({processCssUrls: false})
-    .js('resources/admin/js/admin.js', 'public/admin/js')
-    .sass('resources/admin/sass/admin.scss', 'public/admin/css')
+    .js('resources/admin/js/admin.js', 'public/assets/admin/js')
+    .sass('resources/admin/sass/admin.scss', 'public/assets/admin/css')
     .version();
 
 mix.copy('node_modules/tinymce/skins', 'public/css/skins');
 
-mix.copy('node_modules/blueimp-file-upload/img/loading.gif', 'public/admin/css/img')
-    .copy('node_modules/blueimp-file-upload/img/progressbar.gif', 'public/admin/css/img')
-    .copy('node_modules/blueimp-file-upload/css/jquery.fileupload.css', 'public/admin/css/blueimp')
-    .copy('node_modules/blueimp-file-upload/css/jquery.fileupload-ui.css', 'public/admin/css/blueimp')
-    .copy('node_modules/blueimp-file-upload/css/jquery.fileupload-noscript.css', 'public/admin/css/blueimp')
-    .copy('node_modules/blueimp-file-upload/css/jquery.fileupload-ui-noscript.css', 'public/admin/css/blueimp')
-    .copy('node_modules/blueimp-file-upload/js/vendor/jquery.ui.widget.js', 'public/admin/js/blueimp/vendor')
-    .copy('node_modules/blueimp-tmpl/js/tmpl.js', 'public/admin/js/blueimp')
-    .copy('node_modules/blueimp-load-image/js/load-image.all.min.js', 'public/admin/js/blueimp')
-    .copy('node_modules/blueimp-canvas-to-blob/js/canvas-to-blob.min.js', 'public/admin/js/blueimp')
-    .copy('node_modules/blueimp-file-upload/js/jquery.iframe-transport.js', 'public/admin/js/blueimp')
-    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload.js', 'public/admin/js/blueimp')
-    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-process.js', 'public/admin/js/blueimp')
-    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-image.js', 'public/admin/js/blueimp')
-    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-audio.js', 'public/admin/js/blueimp')
-    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-video.js', 'public/admin/js/blueimp')
-    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-validate.js', 'public/admin/js/blueimp')
-    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-ui.js', 'public/admin/js/blueimp')
-    .copy('node_modules/blueimp-file-upload/js/cors/jquery.xdr-transport.js', 'public/admin/js/blueimp/cors');
+mix.copy('node_modules/blueimp-file-upload/img/loading.gif', 'public/assets/admin/css/img')
+    .copy('node_modules/blueimp-file-upload/img/progressbar.gif', 'public/assets/admin/css/img')
+    .copy('node_modules/blueimp-file-upload/css/jquery.fileupload.css', 'public/assets/admin/css/blueimp')
+    .copy('node_modules/blueimp-file-upload/css/jquery.fileupload-ui.css', 'public/assets/admin/css/blueimp')
+    .copy('node_modules/blueimp-file-upload/css/jquery.fileupload-noscript.css', 'public/assets/admin/css/blueimp')
+    .copy('node_modules/blueimp-file-upload/css/jquery.fileupload-ui-noscript.css', 'public/assets/admin/css/blueimp')
+    .copy('node_modules/blueimp-file-upload/js/vendor/jquery.ui.widget.js', 'public/assets/admin/js/blueimp/vendor')
+    .copy('node_modules/blueimp-tmpl/js/tmpl.js', 'public/assets/admin/js/blueimp')
+    .copy('node_modules/blueimp-load-image/js/load-image.all.min.js', 'public/assets/admin/js/blueimp')
+    .copy('node_modules/blueimp-canvas-to-blob/js/canvas-to-blob.min.js', 'public/assets/admin/js/blueimp')
+    .copy('node_modules/blueimp-file-upload/js/jquery.iframe-transport.js', 'public/assets/admin/js/blueimp')
+    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload.js', 'public/assets/admin/js/blueimp')
+    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-process.js', 'public/assets/admin/js/blueimp')
+    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-image.js', 'public/assets/admin/js/blueimp')
+    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-audio.js', 'public/assets/admin/js/blueimp')
+    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-video.js', 'public/assets/admin/js/blueimp')
+    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-validate.js', 'public/assets/admin/js/blueimp')
+    .copy('node_modules/blueimp-file-upload/js/jquery.fileupload-ui.js', 'public/assets/admin/js/blueimp')
+    .copy('node_modules/blueimp-file-upload/js/cors/jquery.xdr-transport.js', 'public/assets/admin/js/blueimp/cors');
 
+mix.browserSync('mupaes.loc');
