e4c9ae02056d916f5604d3867d6b9c1f5f3496dc
RitchieWhitemore <RichiB@yandex.ru>
2020-06-10 21:13:46 +0300

    Сверстал главную страницу

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{mix('/css/app.css')}}">
     <title>Laravel</title>
@@ -11,55 +12,56 @@
 <body>
 <header class="main-header">
     <div class="main-header__wrapper container">
-        <div class="row no-gutters">
-            <div class="col-6">
-                <div class="main-header__logo-wrapper ">
-                    <a href="/">
-                        <img class="main-header__logo" src="/img/logo.png">
-                    </a>
-                </div>
-            </div>
-            <div class="col-6 d-flex flex-column">
-                <button class="main-header__btn-menu ml-auto">
-                    <div class="main-header__btn-menu-bar1"></div>
-                    <div class="main-header__btn-menu-bar2"></div>
-                    <div class="main-header__btn-menu-bar3"></div>
-                </button>
-                <p>
-                    <a href="" class="main-header__phone">+7 (49244) <span>2-34-34</span></a>
-                </p>
-            </div>
+        <div class="main-header__logo-wrapper ">
+            <a href="/">
+                <img class="main-header__logo" src="/img/logo.png">
+            </a>
         </div>
-        <div class="row no-gutters">
-            <div class="col-6">
-                <p>г. Александров, Красный переулок, д.8</p>
-            </div>
-            <div class="col-6">
-                <p class="text-right">оперативно-диспетчерской служба</p>
-            </div>
-        </div>
+        <p class="main-header__address">г. Александров, Красный переулок, д.8</p>
+        <p class="main-header__phone">
+            <a href="tel:+74924423434">+7 (49244) <span>2-34-34</span></a>
+        </p>
 
-        <nav class="col-12 main-nav main-nav--close">
+        <p class="main-header__service text-right">оперативно-диспетчерской служба</p>
+
+        <button class="main-header__btn-menu ml-auto">
+            <div class="main-header__btn-menu-bar1"></div>
+            <div class="main-header__btn-menu-bar2"></div>
+            <div class="main-header__btn-menu-bar3"></div>
+        </button>
+    </div>
+    <nav class="main-nav main-nav--close">
+        <div class="container">
             <ul class="main-nav__list">
                 <li class="main-nav__item"><a href="" class="main-nav__link main-nav__link--active">Главная</a></li>
-                <li class="main-nav__item"><a href="" class="main-nav__link">Информация</a></li>
-                <li class="main-nav__item"><a href="" class="main-nav__link">Потребителям</a></li>
-                <li class="main-nav__item"><a href="" class="main-nav__link">Закупки</a>
+                <li class="main-nav__item"><a href="" class="main-nav__link">Информация<i class="fas fa-caret-down"></i></a>
+
                     <ul class="main-nav__sub-list">
-                        <li class="main-nav__item"><a href="" class="main-nav__link">Информация о закупочной
+                        <li class="main-nav__sub-item"><a href="" class="main-nav__link">Информация о закупочной
                                 деятельности</a></li>
-                        <li class="main-nav__item"><a href="" class="main-nav__link">Отчеты о закупках</a></li>
-                        <li class="main-nav__item"><a href="" class="main-nav__link">Информация о закупках</a></li>
+                        <li class="main-nav__sub-item"><a href="" class="main-nav__link">Отчеты о закупках</a></li>
+                        <li class="main-nav__sub-item"><a href="" class="main-nav__link">Информация о закупках</a></li>
+                    </ul>
+                </li>
+                <li class="main-nav__item"><a href="" class="main-nav__link">Потребителям</a></li>
+                <li class="main-nav__item"><a href="" class="main-nav__link">Закупки<i
+                            class="fas fa-caret-down"></i></a>
+                    <ul class="main-nav__sub-list">
+                        <li class="main-nav__sub-item"><a href="" class="main-nav__link">Информация о закупочной
+                                деятельности</a></li>
+                        <li class="main-nav__sub-item"><a href="" class="main-nav__link">Отчеты о закупках</a></li>
+                        <li class="main-nav__sub-item"><a href="" class="main-nav__link">Информация о закупках</a></li>
                     </ul>
                 </li>
                 <li class="main-nav__item"><a href="" class="main-nav__link">Контакты</a></li>
             </ul>
-        </nav>
-    </div>
+        </div>
+
+    </nav>
 </header>
 <main>
-    <div class="row container no-gutters">
-        <section class="col-12">
+    <div class="row container no-gutters content-container">
+        <section class="col-12 col-lg-8 pr-lg-5">
             <h1 class="title">Муниципальное унитарное предприятие
                 "Александровэлектросеть"
                 Александровского района</h1>
@@ -84,7 +86,7 @@
                 <img src="/img/image.jpg" class="img-fluid">
             </div>
         </section>
-        <aside class="col-12">
+        <aside class="col-12 col-lg-4">
             <section class="main-news">
                 <h2>Новости и события</h2>
                 <ul>
@@ -114,23 +116,29 @@
     </div>
 </main>
 <footer class="main-footer">
-    <div class="row container no-gutters mb-3">
-        <div class="col-12">
-            <ul class="main-footer__menu-list">
-                <li><a href="">Главная</a></li>
-                <li><a href="">Контакты</a></li>
-                <li><a href="">Раскрытие информации</a></li>
-                <li><a href="">Личный кабинет</a></li>
-                <li><a href="">Галерея</a></li>
-            </ul>
+    <div class="container no-gutters mb-3 m-lg-auto main-footer__container">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <ul class="main-footer__menu-list">
+                    <li><a href="">Главная</a></li>
+                    <li><a href="">Контакты</a></li>
+                    <li><a href="">Раскрытие информации</a></li>
+                    <li><a href="">Личный кабинет</a></li>
+                    <li><a href="">Галерея</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-lg-6 main-footer__desktop-contact">
+                <p class="main-footer__address">г. Александров, Красный переулок, д.8</p>
+                <a tel="+74924423434" class="main-footer__phone">+7 (49244) <span>2-34-34</span></a>
+            </div>
         </div>
-    </div>
-    <div class="row container no-gutters">
-        <div class="col-6">
-            <p>г. Александров, Красный переулок, д.8</p>
-        </div>
-        <div class="col-6 d-flex">
-            <a tel="+74924423434" class="main-footer__phone">+7 (49244) <span>2-34-34</span></a>
+        <div class="main-footer__mobile-contact row">
+            <div class="col-6">
+                <p class="main-footer__address">г. Александров, Красный переулок, д.8</p>
+            </div>
+            <div class="col-6 d-flex">
+                <a tel="+74924423434" class="main-footer__phone">+7 (49244) <span>2-34-34</span></a>
+            </div>
         </div>
     </div>
 </footer>
